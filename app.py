eb201388097be19502b68dadf108cd2e588518f2
22Nathan <64403796+22Nathan@users.noreply.github.com>
2024-03-29 11:01:47 +0100

    :alien:

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-
+import geopandas as gpd
 import leafmap.kepler as leafmap
+# import leafmap
+# import streamlit.components.v1 as components
+# import leafmap.foliumap as leafmap
+
+# st.balloons()
+# st.snow()
 
 geojson = 'geojson/communes-74-haute-savoie.geojson'
 
-with st.echo():
-    m = leafmap.Map(center=[45.9, 6.5], zoom=8)
-    m.add_geojson(geojson, layer_name="zzzz")
-    m.to_streamlit()
+m = leafmap.Map(center=[45.9, 6.5], zoom=8)
+m.add_geojson(
+    geojson, 
+    layer_name='MAP', 
+    style={ 'fillOpacity': .5 }, 
+    fill_colors=["red", "blue"]
+)
+m.to_streamlit()
 
 st.title('L.A. Beat')
